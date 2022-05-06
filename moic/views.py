2904dab38a4444d61d80e704267271a79082eda9
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-05-06 08:10:48 -0400

    added views

--- moic/views.py
+++ moic/views.py
@@ -2,6 +2,42 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'head.html')
 
 def index(request):
-    return render(request, 'bas.html')
+    return render(request, 'bas1.html')
+
+#about us
+def mission(request):
+    return render(request, 'about-us.html')
+
+def dirigeants(request):
+    return render(request, 'features.html')
+
+def conference(request):
+    return render(request, 'pricing.html')
+
+
+
+#our work
+def peace(request):
+    return render(request, 'peace.html')
+
+def simulation(request):
+    return render(request, 'hire-me.html')
+
+def humanity(request):
+    return render(request, 'cv.html')
+
+def sustainable(request):
+    return render(request, 'index2.html')
+
+
+#get involved
+def involved(request):
+    return render(request, 'contact-us.html')
+
+#corrona
+def corona(request):
+    return render(request, 'projects-grid-cards.html')
